afe20a7f9db6fe39c8b06d3da44373c0da3e512f
Matthew Cheok <cheok.jz@gmail.com>
2014-03-30 17:41:38 +0800

    Added MCDateValueTransformer as a convenience NSValueTransformer for MCModel

---
+++ MCAdditions/Classes/Model/MCDateValueTransformer.h
@@ -0,0 +1,16 @@
+//
+//  MCDateValueTransformer.h
+//  Pods
+//
+//  Created by Matthew Cheok on 30/3/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const MCDateValueTransformerName;
+extern NSString * const MCDateTimeValueTransformerName;
+
+@interface MCDateValueTransformer : NSValueTransformer
+
+@end
---
+++ MCAdditions/Classes/Model/MCDateValueTransformer.m
@@ -0,0 +1,55 @@
+//
+//  MCDateValueTransformer.m
+//  Pods
+//
+//  Created by Matthew Cheok on 30/3/14.
+//
+//
+
+#import "MCDateValueTransformer.h"
+#import <ISO8601DateFormatter.h>
+
+NSString * const MCDateValueTransformerName = @"MCDateValueTransformerName";
+NSString * const MCDateTimeValueTransformerName = @"MCDateTimeValueTransformerName";
+
+@interface MCDateValueTransformer ()
+
+@property (strong, nonatomic) ISO8601DateFormatter *formatter;
+
+@end
+
+@implementation MCDateValueTransformer
+
++ (void)load {
+    @autoreleasepool {
+        [NSValueTransformer setValueTransformer:[[self alloc] initWithFormatterDisplayingTime:NO] forName:MCDateValueTransformerName];
+        [NSValueTransformer setValueTransformer:[[self alloc] initWithFormatterDisplayingTime:YES] forName:MCDateTimeValueTransformerName];
+    }
+}
+
+- (instancetype)initWithFormatterDisplayingTime:(BOOL)displaysTime {
+    self = [super init];
+    if (self) {
+        _formatter = [[ISO8601DateFormatter alloc] init];
+        _formatter.includeTime = displaysTime;
+    }
+    return self;
+}
+
++ (Class)transformedValueClass {
+    return [NSDate class];
+}
+
++ (BOOL)allowsReverseTransformation {
+    return YES;
+}
+
+- (id)transformedValue:(id)value {
+    return [self.formatter dateFromString:value];
+}
+
+- (id)reverseTransformedValue:(id)value {
+    return [self.formatter stringFromDate:value];
+}
+
+@end
