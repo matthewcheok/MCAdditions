6ef711ec2bd51aa64609f963ad34c43040e5a60f
Matthew Cheok <cheok.jz@gmail.com>
2014-05-02 07:52:10 +0800

    Updated left aligned layout to account for delegate

--- MCAdditions/Classes/UI/MCCollectionViewAlignedLeftLayout.m
+++ MCAdditions/Classes/UI/MCCollectionViewAlignedLeftLayout.m
@@ -29,7 +29,17 @@ static CGFloat const kLayoutSectionInset = 20;
 }
 
 - (void)setup {
+    self.scrollDirection = UICollectionViewScrollDirectionVertical;
     self.sectionInset = UIEdgeInsetsMake(kLayoutSectionInset, kLayoutSectionInset, kLayoutSectionInset, kLayoutSectionInset);
+}
+
+#pragma mark - Properties
+
+- (void)setScrollDirection:(UICollectionViewScrollDirection)scrollDirection {
+    if (scrollDirection != UICollectionViewScrollDirectionVertical) {
+        [NSException raise:NSInternalInconsistencyException format:@"Only vertical scroll direction is supported."];
+    }
+    [super setScrollDirection:scrollDirection];
 }
 
 #pragma mark - UICollectionViewFlowLayout
@@ -45,6 +55,10 @@ static CGFloat const kLayoutSectionInset = 20;
     return attributesToReturn;
 }
 
+- (id<UICollectionViewDelegateFlowLayout>)delegate {
+    return (id)self.collectionView.delegate;
+}
+
 - (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewLayoutAttributes *currentItemAttributes = [super layoutAttributesForItemAtIndexPath:indexPath];
     CGRect frame = currentItemAttributes.frame;
@@ -52,13 +66,25 @@ static CGFloat const kLayoutSectionInset = 20;
     if (indexPath.item > 0) {
         UICollectionViewLayoutAttributes *previousItemAttributes = [self layoutAttributesForItemAtIndexPath:[NSIndexPath indexPathForItem:indexPath.item-1 inSection:indexPath.section]];
         if (CGRectGetMinX(currentItemAttributes.frame) > CGRectGetMinX(previousItemAttributes.frame)) {
-            frame.origin.x = CGRectGetMaxX(previousItemAttributes.frame) + self.minimumInteritemSpacing;
+            
+            CGFloat minimumInteritemSpacing = self.minimumInteritemSpacing;
+            if ([self.delegate respondsToSelector:@selector(collectionView:layout:minimumInteritemSpacingForSectionAtIndex:)]) {
+                minimumInteritemSpacing = [self.delegate collectionView:self.collectionView layout:self minimumInteritemSpacingForSectionAtIndex:indexPath.section];
+            }
+            
+            frame.origin.x = CGRectGetMaxX(previousItemAttributes.frame) + minimumInteritemSpacing;
             currentItemAttributes.frame = frame;
             return currentItemAttributes;
         }
     }
 
-    frame.origin.x = self.sectionInset.left;
+    if ([self.delegate respondsToSelector:@selector(collectionView:layout:insetForSectionAtIndex:)]) {
+        UIEdgeInsets inset = [self.delegate collectionView:self.collectionView layout:self insetForSectionAtIndex:indexPath.section];
+        frame.origin.x = inset.left;
+    }
+    else {
+        frame.origin.x = self.sectionInset.left;
+    }
     currentItemAttributes.frame = frame;
     return currentItemAttributes;
 }
